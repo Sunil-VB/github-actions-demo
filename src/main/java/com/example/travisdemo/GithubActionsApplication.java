4fe9890a676556c631835a912f794261063028f1
Sunil Vishnu Bhandari <sunil.vb@Triconinfotech.com>
2021-06-23 11:47:36 +0530

    second commit

--- src/main/java/com/example/travisdemo/GithubActionsApplication.java
+++ src/main/java/com/example/travisdemo/GithubActionsApplication.java
@@ -2,12 +2,18 @@ package com.example.travisdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GithubActionsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GithubActionsApplication.class, args);
 	}
 
+	
+	@GetMapping("/hi")
+	public String sayHi() {return "hi";}
 }
